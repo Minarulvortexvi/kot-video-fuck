d4c0431687e8239919a83fe8d3ae76eeb61ab371
Minarulvortex <g57504444@gmail.com>
2025-06-28 01:47:54 +0600

    Create Comment.js

---
+++ backend/models/Comment.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  videoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Video' },
+  text: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
