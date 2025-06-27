552f7b26a6443d25984391e245e219c8fef030b3
Minarulvortex <g57504444@gmail.com>
2025-06-28 01:49:36 +0600

    Create Chat.js

---
+++ backend/models/Chat.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const ChatSchema = new mongoose.Schema({
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  messages: [{
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    text: String,
+    createdAt: { type: Date, default: Date.now }
+  }],
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Chat', ChatSchema);
